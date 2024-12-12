c6997e885320b7876ceb2ab5b95e467ce4af4d52
Pietro <pietrocorona16@gmail.com>
2024-12-11 23:09:30 -0300

    Segundo item: Silly Ingot

--- src/main/java/net/pietro/tutorialmod/item/ModItems.java
+++ src/main/java/net/pietro/tutorialmod/item/ModItems.java
@@ -11,6 +11,7 @@ import net.pietro.tutorialmod.TutorialMod;
 public class ModItems {
 
     public static final Item SILLY_CAT = registerItem("silly_cat", new Item(new Item.Settings()));
+    public static final Item SILLY_INGOT = registerItem("silly_ingot", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -21,6 +22,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(SILLY_CAT);
+            fabricItemGroupEntries.add(SILLY_INGOT);
         });
     }
 }
